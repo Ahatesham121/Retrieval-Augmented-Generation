32196284ec538532fd9d118a399aed9b99e7dec1
Ahatesham121 <87367643+Ahatesham121@users.noreply.github.com>
2026-07-26 20:39:31 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -7,10 +7,10 @@ if __name__ == "__main__":
     
     docs = load_all_documents("data")
     store = FaissVectorStore("faiss_store")
-    #store.build_from_documents(docs)
+    store.build_from_documents(docs)
     store.load()
-    #print(store.query("What is attention mechanism?", top_k=3))
+    print(store.query("how can i take care of my mental health?", top_k=3))
     rag_search = RAGSearch()
-    query = "What is attention mechanism?"
+    query = "how can i take care of my mental health?"
     summary = rag_search.search_and_summarize(query, top_k=3)
     print("Summary:", summary)
